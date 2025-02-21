2b7fb1169a4887471ef682ca92563aac7899c775
onebrownguy <131419488+onebrownguy@users.noreply.github.com>
2025-02-20 23:13:20 -0600

    Updated menu: Added more taco options and improved UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
